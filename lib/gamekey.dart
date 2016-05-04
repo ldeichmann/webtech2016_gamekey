0738e3ca9dec5fe48902999347422d4c4f161879
xPoggi <poggi1@gmx.de>
2016-05-04 11:42:28 +0200

    Aenderungen

--- lib/gamekey.dart
+++ lib/gamekey.dart
@@ -13,5 +13,16 @@ main(){
     app.post('/test').listen((request){
       request.response.send('Affe');
     });
+
+    app.get('/user/:id').listen((request){
+      String pwd = request.params['pwd'];
+      String id = request.params['id'];
+      String byname = request.params['byname'];
+
+
+      if(byname != null && byname!= 'true' && byname != 'false') {
+        request.response.status(HttpStatus.BAD_REQUEST).send("Bad Request: byname parameter must be 'true' or 'false' (if set), was $byname.");
+      }
+    });
   });
 }
